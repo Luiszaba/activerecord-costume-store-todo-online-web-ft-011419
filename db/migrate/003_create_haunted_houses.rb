2584bc44d6581d418383da060c3835e173ce24f6
Luis Zabala <luis.zabala076@gmail.com>
2019-02-21 21:07:03 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,4 +3,11 @@
 class HauntedHouses < ActiveRecord::Migration[4.2]
   
   def change
-    
+    create_table :haunted_houses do |t|
+      t.string :name,
+      t.string :location,
+      t.string :theme,
+      t.float :price,
+      t.string :friendly_?,
+      t.timestamp :opening_time,
+      t.timestamp :closing_time

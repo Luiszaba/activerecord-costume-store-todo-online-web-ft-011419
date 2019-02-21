8e59fd065e95b54df535ddf29e295ab1f8bbd03f
Luis Zabala <luis.zabala076@gmail.com>
2019-02-21 20:26:43 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -2,12 +2,4 @@
 # It should inherit from ActiveRecord::Base
 
 class Costume < ActiveRecord::Base
-  
-  def change
-    create_costumes :costumes do |c|
-      c.string :name,
-      c.integer :price,
-      c.url :url
-    end
-  end
 end

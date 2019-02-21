08f56492b6e568fac84a1716fe18518e9e880760
Luis Zabala <luis.zabala076@gmail.com>
2019-02-21 20:30:10 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,7 +14,7 @@ class CreateCostumes < ActiveRecord::Migration
   def change
     create_table costumes do |c|
       c.string :name,
-      c.integer :price,
+      c.float :price,
       c.url :url
     end
   end

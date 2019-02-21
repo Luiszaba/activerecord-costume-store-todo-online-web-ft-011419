7695bf3a71551c82b4d81f37e4a21b0fb4aae9fb
Luis Zabala <luis.zabala076@gmail.com>
2019-02-21 21:32:47 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,11 +4,11 @@ class CostumeStores < ActiveRecord::Migration[4.2]
   
   def change
     create_table :costume_stores do |t|
-      t.string :name, 
-      t.string :location,
-      t.integer :costume_inventory,
-      t.integer :employees,
-      t.boolean :active,
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees
+      t.boolean :active
       t.timestamp
     end
   end

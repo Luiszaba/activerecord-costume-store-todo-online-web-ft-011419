c2ef1f22e7435674120ebe7ae468f20d5e12e6d0
Luis Zabala <luis.zabala076@gmail.com>
2019-02-21 21:04:35 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,4 +3,14 @@
 class CostumeStores < ActiveRecord::Migration[4.2]
   
   def change
-    create_table costume_stores do |t|
+    create_table :costume_stores do |t|
+      t.string :name, 
+      t.string :location,
+      t.integer :costume_inventory,
+      t.integer :employees,
+      t.string :active,
+      t.timestamp :opening_time,
+      t.timestamp :closing_time
+    end
+  end
+end

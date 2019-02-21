6dea51606ad53065aeb1e11d011a7e21fd75a31a
Luis Zabala <luis.zabala076@gmail.com>
2019-02-21 21:32:58 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,13 +4,13 @@ class HauntedHouses < ActiveRecord::Migration[4.2]
   
   def change
     create_table :haunted_houses do |t|
-      t.string :name,
-      t.string :location,
-      t.string :theme,
-      t.float :price,
-      t.boolean :friendly_?,
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :friendly_?
       t.string :description
-      t.timestamp :opening_time,
+      t.timestamp :opening_time
       t.timestamp :closing_time
     end
   end

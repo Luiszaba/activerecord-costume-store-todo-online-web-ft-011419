5aa2683c0a0f1c5fb5ac87a55c632b70baa88942
Luis Zabala <luis.zabala076@gmail.com>
2019-02-21 21:27:56 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -17,8 +17,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.float :price,
       t.string :image_url,
       t.string :size,
-      t.timestamp :created_at,
-      t.timestamp :updated_at
+      t.timestamp null=false
     end
   end
 end
